bffad3f2a5fc470e595de57252142e0e2169b6f1
kanwu <kanwu@cp-1.ssd-test.spark-pg0.wisc.cloudlab.us>
2017-01-28 14:46:52 -0700

    v0.1 be able to connect, insert and search with core API

---
+++ setmv.sh
@@ -0,0 +1,1 @@
+export PATH=~/apache-maven-3.3.9/bin:$PATH
--- src/main/java/org/neo4j/example/p1.java
+++ src/main/java/org/neo4j/example/p1.java
@@ -1,25 +1,79 @@
 package myproc;
 
 import java.util.List;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.ResourceIterator;
 
-import org.neo4j.procedure.Description;
-import org.neo4j.procedure.Name;
-import org.neo4j.procedure.UserFunction;
+import org.neo4j.procedure.*;
 
 /**
  * This is an example how you can create a simple user-defined function for Neo4j.
  */
 public class p1
 {
-    @UserFunction
-    @Description("example.p1(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
-    public String p1(
+	@Context
+    public GraphDatabaseService graphDB;    
+
+	public enum RelationshipTypes implements RelationshipType {
+        IS_FRIEND_OF,
+        HAS_SEEN;
+    }
+
+    public enum Labels implements Label {
+            USER,
+            MOVIE;
+    }
+
+
+	//@UserFunction
+	@Procedure(value = "myproc.p1", mode = Mode.WRITE)
+	@Description("myproc.p1(['s1','s2',...], delimiter) - join the given strings with the given delimiter.")
+    public void p1(
             @Name("strings") List<String> strings,
             @Name(value = "delimiter", defaultValue = ",") String delimiter) {
-        if (strings == null || delimiter == null) {
-            return null;
-        }
-	System.out.println("T01, here in the new procedure!");
-        return String.join(delimiter, strings);
-    }
+		
+		System.out.println("T01, here in the new procedure!");
+        
+
+		// Create Nodes
+            Transaction tx = graphDB.beginTx();
+            Node steve = graphDB.createNode(Labels.USER);
+            steve.setProperty("name", "Steve");
+            Node tom = graphDB.createNode(Labels.USER);
+            tom.setProperty("name", "Tom");
+
+            Node divergent = graphDB.createNode(Labels.MOVIE);
+            divergent.setProperty("name", "Divergent");
+
+            tx.success();
+
+        // Create Relationship
+            tx = graphDB.beginTx();
+            steve.createRelationshipTo(tom, RelationshipTypes.IS_FRIEND_OF);
+            Relationship relationship = steve.createRelationshipTo(divergent, RelationshipTypes.HAS_SEEN);
+            tx.success();
+
+        // Begin some search
+            // Find all users
+            ResourceIterator<Node> users = graphDB.findNodes( Labels.USER );
+            System.out.println( "Users:" );
+            while( users.hasNext() )
+            {
+                Node user = users.next();
+                System.out.println( "\t" + user.getProperty( "name" ) );
+            }
+
+
+
+
+		// return String.join(delimiter, strings);
+    	return;
+	}
 }
